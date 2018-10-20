00c6888a74894e678231460cdc08298ce75ac051
Anthony Hernandez <ant@antfeedr.com>
2018-10-20 17:13:15 -0400

    Adds logic to distinguish nil from ""
    This commit adds necessary logic to distinguish
    `nil` values from `""` values.

--- lib/digestible_attributes/digester.rb
+++ lib/digestible_attributes/digester.rb
@@ -2,17 +2,29 @@ module DigestibleAttributes
   module Digester
     extend ActiveSupport::Concern
 
+    NULL_ESCAPE = "\u0000".freeze
+    FIELD_ESCAPE = "\uffef".freeze
+    FIELD_SEPARATOR = "\uffff".freeze
+
     def calculate_digest
       OpenSSL::Digest(self.class.digest_algorithm).hexdigest(digestible_string)
     end
 
     private
 
+    def symbolized_digest_fields
+      attributes.symbolize_keys.slice(*self.class.digest_fields)
+    end
+
     def digestible_string
-      attributes.symbolize_keys.
-        slice(*self.class.digest_fields).
-        map { |field, value| "#{field}\uffef#{value.to_s}" }.
-        join("\uffff")
+      symbolized_digest_fields.map do |field, value|
+        base_str = "#{field}#{FIELD_ESCAPE}"
+        if value.nil?
+          base_str += NULL_ESCAPE
+        else
+          base_str += value.to_s
+        end
+      end.join(FIELD_SEPARATOR)
     end
   end
 end
